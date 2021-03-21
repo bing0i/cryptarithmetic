411b08f38e1ff106d989a1f325f3a989c4853b0b
Courgette <courgett3@gmail.com>
2021-03-21 12:02:10 +0700

    Complete backtrack(), isSatisfied() with plus operators and many operands

--- main.py
+++ main.py
@@ -1,35 +1,6 @@
-letters = {
-    "A": -1,
-    "B": -1,
-    "C": -1,
-    "D": -1,
-    "E": -1,
-    "F": -1,
-    "G": -1,
-    "H": -1,
-    "I": -1,
-    "J": -1,
-    "K": -1,
-    "L": -1,
-    "M": -1,
-    "N": -1,
-    "O": -1,
-    "P": -1,
-    "Q": -1,
-    "R": -1,
-    "S": -1,
-    "T": -1,
-    "U": -1,
-    "V": -1,
-    "X": -1,
-    "Y": -1,
-    "W": -1,
-    "Z": -1,
-}
 data = {}
 domains = {}
 variables = []
-constraints = {}
 
 
 def readInputFile(path):
@@ -41,6 +12,15 @@ def readInputFile(path):
     return data
 
 
+def writeOutputFile(path, result):
+    with open(path, "w") as outputFile:
+        if isinstance(result, str):
+            outputFile.write(result)
+        else:
+            outputFile.write("".join([str(v) for v in result.values()]))
+    return True
+
+
 def getVariables(data):
     variables = []
     for operand in data["LHS"]:
@@ -49,40 +29,11 @@ def getVariables(data):
     return list(set(variables))
 
 
-def consistent(variable, assignment):
-    for constraint in constraints[variable]:
-        if not constraint.satisfied(assignment):
-            return False
-    return True
-
-
-# def backtracking_search(assignment):
-#     if len(assignment) == len(variables):
-#         return assignment
-
-#     # get all variables in the CSP but not in the assignment
-#     unassigned = [v for v in variables if v not in assignment]
-
-#     # get the every possible domain value of the first unassigned variable
-#     first = unassigned[0]
-#     for value in domains[first]:
-#         local_assignment = assignment.copy()
-#         local_assignment[first] = value
-#         # if we're still consistent, we recurse (continue)
-#         if consistent(first, local_assignment):
-#             result = backtracking_search(local_assignment)
-#             # if we didn't find the result, we will end up backtracking
-#             if result is not None:
-#                 return result
-#     return None
-
-
 def getDomains(letters):
     possibleDigits = {}
     for letter in letters:
         possibleDigits[letter] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    # no leading 0s
     maxOperandLength = len(data["LHS"][0])
     possibleDigits[data["LHS"][0][0]] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     for operand in data["LHS"][1:]:
@@ -97,6 +48,67 @@ def getDomains(letters):
     return possibleDigits
 
 
+def isSatisfied(assignedVariables):
+    if len(set(assignedVariables.values())) < len(assignedVariables):
+        return False
+
+    if len(assignedVariables) == len(variables):
+        unit = 1
+        result = []
+        tmpSum = 0
+        for index in range(len(data["LHS"])):
+            for letter in reversed(data["LHS"][index]):
+                tmpSum += assignedVariables[letter] * unit
+                unit = unit * 10
+            result.append(tmpSum)
+            tmpSum = 0
+            unit = 1
+
+        rhsSum = 0
+        unit = 1
+        for letter in reversed(data["RHS"]):
+            rhsSum += assignedVariables[letter] * unit
+            unit = unit * 10
+
+        lhsSum = 0
+        for index in range(len(result)):
+            lhsSum += result[index]
+
+        return lhsSum == rhsSum
+
+    return True
+
+
+def backtrack(assignedVariables):
+    if len(assignedVariables) == len(variables):
+        return assignedVariables
+
+    unassignedVariables = [v for v in variables if v not in assignedVariables]
+
+    firstUnassignedVariable = unassignedVariables[0]
+    for value in domains[firstUnassignedVariable]:
+        localAssignedVariables = assignedVariables.copy()
+        localAssignedVariables[firstUnassignedVariable] = value
+        if isSatisfied(localAssignedVariables):
+            result = backtrack(localAssignedVariables)
+            if result is not None:
+                return result
+    return None
+
+
+def sortResult(result):
+    if result == None:
+        return "No solution"
+    sortedKeys = sorted(result.keys())
+    newResult = {}
+    for key in sortedKeys:
+        newResult[key] = result[key]
+    return newResult
+
+
 data = readInputFile("input.txt")
 variables = getVariables(data)
 domains = getDomains(variables)
+
+result = sortResult(backtrack({}))
+writeOutputFile("output.txt", result)
